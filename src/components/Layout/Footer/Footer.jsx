c36a1c77ab92f3bd9d397c098aecfeacffeb99da
Baysangur Elbiev <Nepstor95@gmail.com>
2023-12-29 11:20:54 +0300

    add footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,5 +1,40 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <div className={styles}>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <p className={styles.copyright}>
+            <span>VK</span>© 2006–2020
+          </p>
+          <ul className={styles.links}>
+            <li>
+              <a href="/">About VK</a>
+            </li>
+            <li>
+              <a href="/">Terms</a>
+            </li>
+            <li>
+              <a href="/">Developers</a>
+            </li>
+          </ul>
+          <ul className={styles.links}>
+            <li>
+              <a href="/">English</a>
+            </li>
+            <li>
+              <a href="/">Русский</a>
+            </li>
+            <li>
+              <a href="/">Українська</a>
+            </li>
+            <li>
+              <a href="/">all languages »</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
